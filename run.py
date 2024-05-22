b7b1e46af9b1fd2a663c35f8221fb84ec0e85822
Renlyze <Ultara567@gmail.com>
2024-05-22 19:17:02 +0800

    added more pretrained models

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("python detect.py --weights hehe.pt --img 416 --conf 0.25 --source 1")
+os.system("python detect.py --weights ahihihihi.pt --img 416 --conf 0.8 --source 0")
 
 #python detect.py --weights best.pt --img 416 --conf 0.5 --source 0
